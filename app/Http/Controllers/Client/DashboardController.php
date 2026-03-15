e0dcbc18873052b393df9b5e1204e34114571786
serbanraiescu <serbanraiescu@gmail.com>
2026-03-15 14:11:33 +0200

    Fix 500 error in DashboardController due to missing Client import

--- app/Http/Controllers/Client/DashboardController.php
+++ app/Http/Controllers/Client/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Booking;
+use App\Models\Client;
 
 class DashboardController extends Controller
 {
